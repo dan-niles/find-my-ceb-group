15bb1ca3b483e31bfcc0f6c2820ee6f243d1dc58
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2022-05-13 23:59:29 +0530

    Fix marker issue

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -29,6 +29,12 @@ const Layout = (props) => {
 				<title>Find My CEB Group</title>
 				<meta name="description" content="Find Your CEB Group Easily" />
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+					integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+					crossorigin=""
+				/>
 			</Head>
 			{props.children}
 		</ThemeProvider>
